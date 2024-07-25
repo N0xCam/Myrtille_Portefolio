47e8aef1207ce50cc9e0e355a78d0e9cfc44596b
N0xCam <167630075+N0xCam@users.noreply.github.com>
2024-07-25 11:32:00 +0200

    arborescence

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheHeader from '../components/TheHeader.vue';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TheHeader />
   </main>
 </template>
